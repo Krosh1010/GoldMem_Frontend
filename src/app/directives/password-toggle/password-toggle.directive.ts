5d1f9c877c5f5966ebe23e87f6508506fda70665
Krosh1010 <petru.altuhov@gmail.com>
2025-05-08 01:01:10 +0300

    dark/light theme

--- src/app/directives/password-toggle/password-toggle.directive.ts
+++ src/app/directives/password-toggle/password-toggle.directive.ts
@@ -1,38 +1,48 @@
 import {
-  Directive, ElementRef, Renderer2, HostListener, Input, AfterViewInit
+  Directive, ElementRef, Renderer2, HostListener, Input, AfterViewInit,
+  Inject,
+  Injectable,
+  PLATFORM_ID
 } from '@angular/core';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 
 @Directive({
   selector: '[appPasswordToggle]',
 })
+@Injectable()
 export class PasswordToggleDirective implements AfterViewInit {
   @Input() targetInputId?: string;
   private inputElement?: HTMLInputElement;
   private isVisible = false;
   private iconElement?: HTMLElement;
 
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
+  constructor(
+    private el: ElementRef, 
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
   ngAfterViewInit(): void {
-  
-    this.inputElement = this.targetInputId
-      ? document.getElementById(this.targetInputId) as HTMLInputElement
-      : this.el.nativeElement.previousElementSibling;
+    // Only run this code in the browser
+    if (isPlatformBrowser(this.platformId)) {
+      this.inputElement = this.targetInputId
+        ? this.document.getElementById(this.targetInputId) as HTMLInputElement
+        : this.el.nativeElement.previousElementSibling;
 
-    if (!this.inputElement) return;
+      if (!this.inputElement) return;
 
-    
-    this.iconElement = this.renderer.createElement('i');
-    this.renderer.addClass(this.iconElement, 'bi');
-    this.renderer.addClass(this.iconElement, 'bi-eye-slash');
-    this.renderer.addClass(this.iconElement, 'icon');
+      this.iconElement = this.renderer.createElement('i');
+      this.renderer.addClass(this.iconElement, 'bi');
+      this.renderer.addClass(this.iconElement, 'bi-eye-slash');
+      this.renderer.addClass(this.iconElement, 'icon');
 
-    const wrapper = this.el.nativeElement;
-    this.renderer.appendChild(wrapper, this.iconElement);
+      const wrapper = this.el.nativeElement;
+      this.renderer.appendChild(wrapper, this.iconElement);
 
-    
-    this.inputElement.type = 'password';
-    this.renderer.setStyle(wrapper, 'cursor', 'pointer');
+      this.inputElement.type = 'password';
+      this.renderer.setStyle(wrapper, 'cursor', 'pointer');
+    }
   }
 
   @HostListener('click')
